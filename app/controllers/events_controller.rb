ded84c05150fa43facde4ad81fa7ff6a7bd9cd57
Neil Gautam <neilgautam.24@tamu.edu>
2024-10-24 21:55:36 -0500

    rspec cases added

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -23,7 +23,8 @@ class EventsController < ApplicationController
         row.each { |cell| row_data << cell.value }
         @event_box_office_data << row_data
       end
-  
+      
+      puts @event_box_office_data
       Rails.logger.info "Event Box Office Data: #{@event_box_office_data.inspect}"
   
       @referral_data = Referral.where(event_id: @event.id)
