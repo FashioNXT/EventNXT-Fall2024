07ea2bc8822edb97de6740f2d78a3aeb9eed15c4
Surya Cherukuri <suryacherukuri@Suryas-MacBook-Pro-2.local>
2024-11-06 10:36:23 -0600

    Update email configuration in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,25 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  # Ignore bad email addresses and do not raise email delivery errors.
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  config.action_mailer.raise_delivery_errors = true
 
+  # Configure ActionMailer for email delivery using SendGrid
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: ENV['APP_URL'], # Ensure this matches your Heroku app URL or custom domain
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
+  # Default URL options for ActionMailer
+  config.action_mailer.default_url_options = { host: ENV['APP_URL'], protocol: 'https' }
+  
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
